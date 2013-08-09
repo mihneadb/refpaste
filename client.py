957c3a1b66e0b72b5f4b4130deac120677e38a9b
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-08-09 16:07:05 -0700

    add support for linux and safety check for other platforms in paste logic

--- client.py
+++ client.py
@@ -13,8 +13,13 @@ def get_ext(path):
     return ext
 
 def save_in_clipboard(string):
+    # credits to github.com/Raynes/rapture
     if sys.platform == 'darwin':
         cmd = 'pbcopy'
+    elif 'linux' in sys.platform:
+        cmd = 'xclip -selection clipboard'
+    else:
+        raise NotImplementedError("Platform not supported.")
 
     p = Popen([cmd], stdin=PIPE)
     p.communicate(input=string)
@@ -55,8 +60,13 @@ def main():
     if args.no_clipboard:
         print "Pasted at %s." % url
     else:
-        save_in_clipboard(url)
-        print "Saved %s in your clipboard." % url
+        try:
+            save_in_clipboard(url)
+        except NotImplementedError:
+            print "Don't know how to save to clipboard on your platform."
+            print "Pasted at %s." % url
+        else:
+            print "Saved %s in your clipboard." % url
 
 
 if __name__ == '__main__':
